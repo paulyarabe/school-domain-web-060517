d1516d08e71fecb168e731bbf8853855b72c51d4
github username <github email address>
2017-06-08 16:45:13 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+require 'pry-byebug'
+
+class School
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def roster
+		@roster
+	end
+
+
+	def add_student(name,grade)
+		if @roster.keys.include?(grade)
+			@roster[grade] << name
+		else
+			@roster[grade] = []
+			@roster[grade] << name
+		end
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		@roster.collect do |grade, names_array|
+			@roster[grade] = names_array.sort
+		end
+		roster
+	end
+end
+
+
+
+school = School.new("HiSchool")
+school.add_student("john", 10)
+school.add_student("jeff", 10)
+
